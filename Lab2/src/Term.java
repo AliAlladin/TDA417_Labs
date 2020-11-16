68ef4e79f1dea5b3a5a08059ea1b8d4da57b4158
Ali Alladin <ali.alladin20@gmail.com>
2020-11-16 10:10:38 +0100

    Worked with part 1 of the lab.

--- Lab2/src/Term.java
+++ Lab2/src/Term.java
@@ -23,27 +23,42 @@ public class Term {
 
     // Extracts a prefix from the word.
     public String getPrefix(int len) {
-        /* TODO */
-        return null;
+        return String.copyValueOf(word.toCharArray(), 0, len);
+
     }
 
     // Compares the two terms in case-insensitive lexicographic order.
     public static Comparator<Term> byLexicographicOrder() {
         /* TODO */
-        return null;
+        return new Comparator<Term>() {
+            @Override
+            public int compare(Term o1, Term o2) {
+                return o1.word.compareToIgnoreCase(o2.word);
+            }
+        };
     }
 
     // Compares the two terms in descending order by weight.
     public static Comparator<Term> byReverseWeightOrder() {
         /* TODO */
-        return null;
+        return new Comparator<Term>() {
+            @Override
+            public int compare(Term o1, Term o2) {
+                return Long.compare(o1.weight, o2.weight);
+            }
+        };
     }
 
     // Compares the two terms in case-insensitive lexicographic order,
     // but using only the first k characters of each word.
     public static Comparator<Term> byPrefixOrder(int k) {
         /* TODO */
-        return null;
+        return new Comparator<Term>() {
+            @Override
+            public int compare(Term o1, Term o2) {
+                return o1.getPrefix(k).compareToIgnoreCase(o2.getPrefix(k));
+            }
+        };
     }
 
     // Returns a string representation of this term in the following format:
